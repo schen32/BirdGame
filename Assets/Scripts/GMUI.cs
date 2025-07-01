64bd83f03a8591f5ba986738d955a07bd454981a
Simon Chen <simonc8192@gmail.com>
2025-07-01 02:31:33 -0700

    - pause menu

---
+++ Assets/Scripts/GMUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GMUI : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    bool isPaused = false;
+    public void OnPause(InputValue value)
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
