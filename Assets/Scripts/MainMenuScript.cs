72fff156fde2c4581c55775daee2a5995afd3850
Simon Chen <simonc8192@gmail.com>
2025-07-01 15:27:04 -0700

    - can go between main menu and options screen

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
+    public GameObject mainMenu;
     public void PlayGame()
     {
         SceneManager.LoadScene("Level01"); // or use index: SceneManager.LoadScene(1);
@@ -13,9 +14,15 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void Options()
+    public void GoToOptions()
     {
+        mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
-        gameObject.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
